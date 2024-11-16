ceb6177a2959967667234679161a43be0ed6899f
Rafael Fernandes <rafaelbenzi@estudante.ufscar.br>
2024-11-16 01:32:22 +0100

    Line added in Shapes

--- fish_test.py
+++ fish_test.py
@@ -1,1 +1,58 @@
-from animation_tests import Circle
+from shapes import Circle, Dot, Line
+import pygame
+import sys
+from colors import *
+
+# Define the screen size
+screen_size = screen_width, screen_height = 1080, 720
+
+# Define fps limit
+FPS = 30
+
+# Define cursor type
+cursor = pygame.cursors.broken_x
+
+# Setting display and getting the surface object
+pygame.init()
+pygame.mouse.set_cursor(cursor)
+screen = pygame.display.set_mode(screen_size)
+clock = pygame.time.Clock()
+circles = []
+for i in range(3):
+    circles.append(Circle((250,100*i), 20, white, 5))
+my_line = Line((100,100), (300,300), 5)
+
+def main():
+
+    my_circle = Circle((250, 250), 20, white, 0)
+
+    while True:
+        #Limit FPS
+        clock.tick(FPS)
+
+        # Condition to clase the game
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+        
+        # Get mouse position
+        mouse_pos = pygame.mouse.get_pos()
+
+        # Clear the screen
+        screen.fill(black)
+
+        # draw circle in current mouse position and display
+        my_circle.center = mouse_pos
+        #my_circle.display()
+        my_line.display()
+
+        for i, circle in enumerate(circles):
+            circle.center = tuple(a+20*i for a in mouse_pos)
+            circle.display()
+
+        # Display everything in the screen
+        pygame.display.flip()
+
+if __name__ == '__main__':
+    main()
--- shapes.py
+++ shapes.py
@@ -43,7 +43,7 @@ class Dot:
 
         **color**: *list* -> (0-255, 0-255, 0-255)\n
         The color composing a 1x3 list from 0 to 255.
-        
+
         ## Methods:
         **display**: *None*\n
         Displays the dot in the active surface (screen).
@@ -54,3 +54,75 @@ class Dot:
 
     def display(self):
         pygame.draw.circle(self.screen, self.color, self.center, width=0, radius=2)
+
+class Line:
+    def __init__(self, start_pos:List[int], end_pos:List[int], width:Optional[int]=1, color:Optional[List[int]]=(255,255,255)) -> None:
+        """
+        Creates a line to be used with 'pygame' library.
+        ## Atributes:
+        **start_pos**: *list* -> (x, y)\n
+        The start position of the line.
+
+        **end_pos**: *int*\n
+        The ending position of the line.
+
+        **color**: *list* -> (0-255, 0-255, 0-255)\n
+        The color composing a 1x3 list from 0 to 255.
+
+        **width**: *optional, int*\n
+        if = 0, circle will be totally filled.\n
+        if none is specified, it will be set to 1.
+
+        ## Methods:
+        **display**: *None*\n
+        Displays the circle in the active surface (screen).
+        """
+        self.start_pos = start_pos
+        self.end_pos = end_pos
+        self.width = width
+        self.color = color
+        self.screen = pygame.display.get_surface()
+
+    def display(self):
+        pygame.draw.line(self.screen, self.color, self.start_pos, self.end_pos, self.width)
+        
+
+class Chain:
+    def __init__(self, center:List[int], nb_nodes:int, dist:Optional[float]=50, color:Optional[List[int]]=(255,255,255), size:Optional[int]=5) -> None:
+        """
+        Creates a circle to be used with 'pygame' library.
+        ## Atributes:
+        **center**: *list* -> (x, y)\n
+        The center position of the cicle.
+
+        **size**: *int*\n
+        The radius of the circle.
+
+        **color**: *list* -> (0-255, 0-255, 0-255)\n
+        The color composing a 1x3 list from 0 to 255.
+
+        **width**: *optional, int*\n
+        if = 0, circle will be totally filled.\n
+        if none is specified, it will be set to 1.
+
+        ## Methods:
+        **display**: *None*\n
+        Displays the circle in the active surface (screen).
+        """
+        self.center = center
+        self.nb_nodes = nb_nodes
+        self.dist = dist
+        self.size = size if size <= dist else dist
+        self.color = color
+        self.width = 0
+        self.screen = pygame.display.get_surface()
+
+    def display(self):
+        circle = []
+        lines = []
+        for i in range(self.nb_nodes):
+            circle.append(Circle(self.center, self.size, self.color, self.width))
+            circle[0].display()
+            
+
+    
